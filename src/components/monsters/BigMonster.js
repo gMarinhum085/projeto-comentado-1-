00cc3ff1868da80f4c20320998a6460100a42036
Gabriel Marinho dos Santos <152182959+gMarinhum085@users.noreply.github.com>
2024-08-28 16:40:12 -0300

    Add files via upload

---
+++ src/components/monsters/BigMonster.js
@@ -0,0 +1,9 @@
+class BigMonster extends Character {
+    constructor(){
+        super('Big Monster')
+        this.life= 80
+        this.attack = 15 
+        this.defense = 5
+        this.maxLife = this.life
+    }
+}
